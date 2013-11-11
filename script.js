f6d0fc1b65e2d8a928c2aec7018e8f9e630eb122
holytaco <jan.richter@gmail.com>
2013-11-11 10:28:20 +0100

    working with variables

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 
 // this is a line comment
 
-alert("Hello, world.");
+//alert("Hello, world1.");
+
+
+var apples = prompt('enter number of apples:');
+var pears = prompt('enter number of pears:');
+
+
+var piecesOfFruit = apples+pears;
+var piecesForEachPerson = piecesOfFruit/3;
+
+if (apples>pears) {
+    //more apples than pears
+    alert("more apples than pears")
+} else {
+    //equal or less apples than pears
+    alert("equal or less apples than pears")
+}
+
+
+if (piecesForEachPerson<10) {
+    alert("not enough fruit!")
+}
 
 
